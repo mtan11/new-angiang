f9aac4030ec683f32a24ad6f66c3a2aa478dc7f3
tgbao <tgbao@vlab.tech>
2019-12-15 18:16:49 +0700

    add api update data

--- routes/api.php
+++ routes/api.php
@@ -18,3 +18,4 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 });
 
 Route::post('/insert-data-point', 'PostgresController@insertData');
+Route::post('/upload-shapefile','PostgresController@uploadShapeFile');
