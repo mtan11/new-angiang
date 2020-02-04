30973b5bd417cda7ee86633f2a5ea1e9e67f1a81
VLAB <vlab.urbansolution@gmail.com>
2020-02-04 12:10:11 +0700

    fix image legend

--- resources/views/edit.blade.php
+++ resources/views/edit.blade.php
@@ -456,22 +456,22 @@
                     <ul id="myUL">
                         <li><span class="caret">Biến đổi lòng dẫn</span>
                             <ul class="nested">
-                            <img src="/img/legend_biendoilongdan.png">
+                            <img src="/images/legend_biendoilongdan.png">
                             </ul>
                         </li>
                         <li><span class="caret">Độ sâu địa hình đáy</span>
                             <ul class="nested">
-                            <img src="/img/legend_dem.png">
+                            <img src="/images/legend_dem.png">
                             </ul>
                         </li>
                         <li><span class="caret">Diễn biến đường bờ bằng phương pháp viễn thám & GIS qua các năm</span>
                             <ul class="nested">
-                            <img src="/img/legend_duongbo_gis.png">
+                            <img src="/images/legend_duongbo_gis.png">
                             </ul>
                         </li>
                         <li><span class="caret">Dự báo sạt lở đường bờ bằng mô hình</span>
                             <ul class="nested">
-                            <img src="/img/legend_ruiro_satlo.png">
+                            <img src="/images/legend_ruiro_satlo.png">
                             </ul>
                         </li>
                     </ul>
